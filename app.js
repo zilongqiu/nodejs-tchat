8bb1c8c3700bca3b5d60f4007a14464f0d9f6166
Zi-long QIU <zilong.qiu@gmail.com>
2015-02-22 01:10:58 +0100

    Basic socket.io implementation

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var socket = require('socket.io');
+var http = require('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var server = http.createServer(app).listen(8080);
+var io = socket.listen(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +61,11 @@ app.use(function(err, req, res, next) {
     });
 });
 
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 module.exports = app;
