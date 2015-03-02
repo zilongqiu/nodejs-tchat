96e89655684ccfde1918cea46d0979edebad873c
Zi-long QIU <zilong.qiu@gmail.com>
2015-03-02 23:04:00 +0100

    Remove model and mongoose

--- app.js
+++ app.js
@@ -6,18 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var socket = require('socket.io');
 var http = require('http');
-var passport = require('passport');
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    bcrypt = require('bcrypt-nodejs'),
-    SALT_WORK_FACTOR = 10;
-
-// Mongoose
-mongoose.connect('mongodb://localhost:27017/nodejs-tchat', function (error) {
-    if (error) {
-        console.log(error);
-    }
-});;
 
 // Routing
 var routes = require('./routes/index');
@@ -42,12 +30,6 @@ var server = http.createServer(app).listen(8000);
 var io = socket.listen(server);
 require('./modules/user/user.js').initialize(io);
 
-// required for passport
-app.use(session({ secret: '45145fad89214d1c0b50f66fadde7a6d0e885012' }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -60,12 +42,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    req.passport = passport;
-    next();
-});
+
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var db = req.db;
-    db.collection('users').find().toArray(function (err, items) {
-        res.json(items);
-    });
+
 });
 
 module.exports = router;
